ebfe209e2dbd44c903651304d87a86e36b2ad335
Olivia Lammam <olivia.l7475@gmail.com>
2022-05-17 21:56:18 -0700

    password not working in server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'olivia.l7475@gmail.com',
+      // Your MySQL password
+      password: 'Hairyreptile7676!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
