19983d49ee82d783b8cc11adaf20e443bb3c994d
Olivia Lammam <olivia.l7475@gmail.com>
2022-05-17 22:15:52 -0700

    password working

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 // Connect to database
 const db = mysql.createConnection(
     {
@@ -20,6 +16,14 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
   );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+  // Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
